6ebe71b312d810bf55e659f56fa8bbd7c2dacc6d
Malkon F <sudo777@protonmail.com>
2019-04-10 22:18:20 -0300

    Verificando se a atualização com apt-get houve algum erro

--- upgrade.sh
+++ upgrade.sh
@@ -23,5 +23,12 @@ HORA=$(date +"%T, %d/%m/%y, %A")
     sleep 3
     sudo apt-get update && apt-get -y upgrade && apt-get -y dist-upgrade &&
     apt-get -y autoremove && apt-get -y clean && apt-get purge $(dpkg -l | awk '/^rc/ { print $2 }')
-    echo "Atualização feita com sucesso" 
+    if [ $? -eq 0 ]
+    then
+	echo "\033[01;32mAtualização feita com sucesso \033[0m"
+    else
+	echo "\033[31mErro! Sistema não pode ser atualizado\033[0m"
+	exit 1
+    fi
+   
     exit

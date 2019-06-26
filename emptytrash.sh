c663c29cba1f81fea83209d218c3ef397542ad6c
sudo777 <sudo777@protonmail.com>
2019-06-26 19:19:32 +0000

    Adicionando licença e descrição uso

--- emptytrash.sh
+++ emptytrash.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+
+##***********************************************************************************
+#
+# Este script limpa a lixeira do usuário logado. 
+# ----------------------------------
+#
+# Para executar o script: chmod u+x emptytrash.sh && ./emptytrash.sh
+#
+# Author: Malkon F
+# Website: https://www.malkon.me
+# Github: https://github.com/MalkonF
+#
+# This script comes with ABSOLUTELY NO WARRANTY. This is free software, and you are
+# welcome to redistribute it under the terms of the GNU General Public License.
+# See LICENSE file for usage of this software.
+#
+# This script is licensed under GPLv3.
+#
+##***********************************************************************************
+
 main()
 {
 echo "Nunca use com root ou usuário com permissão no sudo"

72e4b4a142ad1103cc0ce83e4f621bedb6d91e21
MalkonF <sudo777@protonmail.com>
2019-08-06 21:42:17 -0300

    Colocando aspas duplas nas variáveis - Boas práticas de programação

--- backup.sh
+++ backup.sh
@@ -34,7 +34,7 @@ See the LICENSE file for details about using this software.\n\n" || echo "/bin/s
 
 echo "\033[1mEnter the directory path to backup:\033[0m\c"
 read -r sbackup
-if [ -f $sbackup ]
+if [ -f "$sbackup" ]
 then
 	echo " \033[31mYou indeed entered a directory name\033[0m" 
 	exit 1
@@ -42,13 +42,13 @@ fi
 
 echo "\033[1mEnter the directory path where the files will be saved:\033[0m\c"
 read -r dbackup
-if [ -f $dbackup ]
+if [ -f "$dbackup" ]
 then
         echo " \033[31mYou indeed entered a directory name\033[0m"
 	exit 1
 fi
 
-if [ -w $dbackup ]
+if [ -w "$dbackup" ]
 then
 	# Create archive filename.
 	day=$(date +%Y-%m-%d-%I-%M)
@@ -56,13 +56,13 @@ then
 	file="$hostname-$day.tgz"
 	
 	#Calculate size available
-	size=$(df -Ph $dbackup | tail -1 | awk '{print $4}')
-	printf "\nThe size available of %s is %s. Do you want to delete all files in folder?[y/n]?\n" $dbackup $size
+	size=$(df -Ph "$dbackup" | tail -1 | awk '{print $4}')
+	printf "\nThe size available of %s is %s. Do you want to delete all files in folder?[y/n]?\n" "$dbackup" "$size"
 	read -r ans
 		if [ "$ans" = "y" ] || [ "$ans" = "Y" ]
 		then
 		        #Removing previous backup's
-			rm -rf $dbackup/*
+			rm -rf "$dbackup"/*
 
 			if [ $? -eq 0 ]
 			then
@@ -73,16 +73,16 @@ then
 			fi
 		fi
 
-	printf "\nBacking up %s to %s/%s..." $sbackup $dbackup $file
+	printf "\nBacking up %s to %s/%s..." "$sbackup" "$dbackup" "$file"
 
 	# Backup the files using tar.
-	tar czf  $dbackup/$file $sbackup
+	tar czf  "$dbackup"/"$file" "$sbackup"
 
 	if [ $? -eq 0 ]
 	then
 		#Check file sizes.
-		size_backup=$(ls -lha $dbackup | tail -1 | awk '{print $5}')
-		printf "\nThe size of backup is: %s \n\n" $size_backup
+		size_backup=$(ls -lha "$dbackup" | tail -1 | awk '{print $5}')
+		printf "\nThe size of backup is: %s \n\n" "$size_backup"
 		printf "\033[01;32mBackup finished successfully.\n\n\033[0m"
 	else
 		echo "\033[31mBackup error!\033[0m"
@@ -94,5 +94,5 @@ else
 fi
 
 echo "Thanks for using this script"
-printf "%s - %s\n" $AUTHOR $AUTHOR_CONTACT
+printf "%s - %s\n" "$AUTHOR" "$AUTHOR_CONTACT"
 

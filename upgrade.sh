c5d5791a975cba650b5b42f204370081e8f2e0d1
MalkonF <sudo777@protonmail.com>
2019-08-06 22:59:30 -0300

    Colocando aspas duplas para prevenir quebra de palavras quando a string contem espaços / Deletando sudo dentro do script

--- upgrade.sh
+++ upgrade.sh
@@ -21,15 +21,14 @@
 [ -f /bin/sh ] && printf "\n\n This script comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to redistribute it under the terms of the GNU General Public License.
  See the LICENSE file for details about using this software.\n\n" || echo "/bin/sh not found" exit 1
 
-NOME=$(whoami) 
-DATA=$(date +'%d/%m/%Y')
-HORA=$(date +'%r')
+NOME="$(whoami)" 
+DATA="$(date +'%d/%m/%Y')"
+HORA="$(date +'%r')"
     printf "Atualizando o sistema com o usuário %s" "$NOME"
     printf "\nData: %s" "$DATA" 
     printf "\nHorário: %s\n" "$HORA"
-    sleep 3
-    sudo apt-get update && apt-get -y upgrade && apt-get -y dist-upgrade &&
-    apt-get -y autoremove && apt-get -y clean && apt-get purge $(dpkg -l | awk '/^rc/ { print $2 }')
+    sleep 2
+    
     if [ $? -eq 0 ]
     then
 	echo "\033[01;32mAtualização feita com sucesso \033[0m"

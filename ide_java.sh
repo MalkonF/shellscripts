428ab85b22a5b8da6fbdeedff40b2825e08d24e1
anonymous <sudo777@protonmail.com>
2019-11-05 20:33:09 -0300

    Refatorando

--- ide_java.sh
+++ ide_java.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-#***********************************************************************************
+#****************************************************************************************************
 #
-# This script installs Netbeans, SpringTool Suite on Debian / GNU Linux systems.
-# ----------------------------------
-#
+# This script installs Netbeans, SpringTool Suite and/or IntelliJ IDEA on Debian / GNU Linux systems.
 # To run the script: chmod u+x ide_java.sh && sudo sh ide_java.sh
 #
-# Author: Malkon F
+# Dependencies: JDK
+#
+# Author:  Malkon F
 # Website: https://www.malkon.me
-# GitHub: https://github.com/MalkonF
+# GitHub:  https://github.com/MalkonF
+# Twitter: https://twitter.com/MalkonF
 #
 # This script comes with ABSOLUTELY NO WARRANTY. This is free software, and you are
 # welcome to redistribute it under the terms of the GNU General Public License.
@@ -16,14 +17,55 @@
 #
 # This script is licensed under GPLv3.
 #
-#***********************************************************************************
-mkdir -p /opt/netbeans
-wget -P /opt/netbeans https://www-eu.apache.org/dist/netbeans/netbeans/11.1/Apache-NetBeans-11.1-bin-linux-x64.sh
-chmod +x /opt/netbeans/Apache-NetBeans-11.1-bin-linux-x64.sh
-cd /opt/netbeans/ || exit
-./Apache-NetBeans-11.1-bin-linux-x64.sh
+#****************************************************************************************************
 
-wget -P /opt https://download.springsource.com/release/STS4/4.3.1.RELEASE/dist/e4.12/spring-tool-suite-4-4.3.1.RELEASE-e4.12.0-linux.gtk.x86_64.tar.gz
-tar -vzxf /opt/spring-tool-suite-4-4.3.1.RELEASE-e4.12.0-linux.gtk.x86_64.tar.gz -C /opt
-cd /opt/sts-4.3.1.RELEASE/ || exit
-./SpringToolSuite4
+SCRIPT_NAME=$(basename $0)
+
+printusage()
+{
+   echo "Usage:"
+           echo "${SCRIPT_NAME} install netbeans"
+           echo "${SCRIPT_NAME} install spring"
+	   echo "${SCRIPT_NAME} install Intellij"
+	   echo "${SCRIPT_NAME} remove netbeans"
+	   echo "${SCRIPT_NAME} remove spring"
+	   echo "${SCRIPT_NAME} remove intellij"
+	
+}
+
+install_netbeans()
+{
+   mkdir -p /opt/netbeans
+   wget -P /opt/netbeans https://www-eu.apache.org/dist/netbeans/netbeans/11.1/Apache-NetBeans-11.1-bin-linux-x64.sh
+   chmod +x /opt/netbeans/Apache-NetBeans-11.1-bin-linux-x64.sh
+   cd /opt/netbeans/ || exit
+   ./Apache-NetBeans-11.1-bin-linux-x64.sh
+}
+
+install_spring()
+{
+   wget -P /opt https://download.springsource.com/release/STS4/4.3.1.RELEASE/dist/e4.12/spring-tool-suite-4-4.3.1.RELEASE-e4.12.0-linux.gtk.x86_64.tar.gz
+   tar -vzxf /opt/spring-tool-suite-4-4.3.1.RELEASE-e4.12.0-linux.gtk.x86_64.tar.gz -C /opt
+   cd /opt/sts-4.3.1.RELEASE/ || exit
+   ./SpringToolSuite4
+}
+
+case "$1" in
+   "-install" )
+   case "$2" in
+       "netbeans" )
+       install_netbeans
+       ;;
+       "spring" )
+       install_spring
+       ;;
+   esac
+   ;;
+      * )
+         echo "Invalid command: $1"
+         printusage
+         exit 1
+         ;;
+esac
+
+
